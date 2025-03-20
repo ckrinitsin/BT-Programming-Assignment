655b46c495f5eda0c3eaa9dd9b8fa8fe1d87b1f4
Christian Krinitsin <mail@krinitsin.com>
2025-03-20 14:06:44 +0100

    server: add concurrency and edit gitignore

--- src/server/hashtable.h
+++ src/server/hashtable.h
@@ -3,7 +3,9 @@
 #include <algorithm>
 #include <iostream>
 #include <list>
+#include <mutex>
 #include <optional>
+#include <shared_mutex>
 #include <vector>
 
 template <typename K, typename V>
@@ -12,12 +14,16 @@ public:
     HashTable(size_t size)
         : size { size }
         , table(size)
+        , bucket_mutexes(size)
     {
     }
 
     bool insert(K key, V value)
     {
-        std::list<std::pair<K, V>>& list = get_bucket(key);
+        size_t index = get_bucket_index(key);
+        std::unique_lock<std::shared_mutex> lock(bucket_mutexes.at(index));
+
+        std::list<std::pair<K, V>>& list = table.at(index);
 
         if (bucket_contains_key(list, key)) {
             return false;
@@ -30,7 +36,10 @@ public:
 
     std::optional<V> get(K key)
     {
-        std::list<std::pair<K, V>>& list = get_bucket(key);
+        size_t index = get_bucket_index(key);
+        std::shared_lock<std::shared_mutex> lock(bucket_mutexes.at(index));
+
+        std::list<std::pair<K, V>>& list = table.at(index);
 
         auto iter = bucket_find_key(list, key);
         if (iter != list.end()) {
@@ -42,7 +51,10 @@ public:
 
     bool remove(K key)
     {
-        std::list<std::pair<K, V>>& list = get_bucket(key);
+        size_t index = get_bucket_index(key);
+        std::unique_lock<std::shared_mutex> lock(bucket_mutexes.at(index));
+
+        std::list<std::pair<K, V>>& list = table.at(index);
 
         auto iter = bucket_find_key(list, key);
         if (iter != list.end()) {
@@ -57,11 +69,13 @@ public:
     {
         size_t index { 0 };
         for (auto bucket : table) {
-            std::cout << "Bucket " << index++ << ": [";
+            std::cout << "Bucket " << index << ": [";
+            std::shared_lock<std::shared_mutex> lock(bucket_mutexes.at(index));
             for (auto pair : bucket) {
                 std::cout << "(" << pair.first << ", " << pair.second << ")";
             }
             std::cout << "]" << "\n";
+            ++index;
         }
     }
 
@@ -69,6 +83,8 @@ private:
     size_t size;
 
     std::vector<std::list<std::pair<K, V>>> table;
+
+    std::vector<std::shared_mutex> bucket_mutexes;
 
     std::hash<K> hash_function;
 
@@ -84,9 +100,5 @@ private:
         return list.begin() != list.end() && bucket_find_key(list, key) != list.end();
     }
 
-    std::list<std::pair<K, V>>& get_bucket(K key)
-    {
-        size_t index = hash_function(key) % size;
-        return table.at(index);
-    }
+    size_t get_bucket_index(K key) { return hash_function(key) % size; }
 };
